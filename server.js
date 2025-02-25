f317d3b32f40ff47e9e2b0aecbbcbcdae214fe3f
zhandos <zhandosorymbetov@gmail.com>
2025-02-25 05:08:27 +0500

    git init:second commit modified PORT settings for deployment

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5003;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => 
+app.listen(PORT, () =>
     console.log(`Server started on port ${PORT}`)
 );
